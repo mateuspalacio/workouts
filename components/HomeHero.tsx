72663e4a9cb0f6c2d98e318bcb038e10ab11875d
Mateus <mateuspalacio@gmail.com>
2025-05-17 21:08:56 -0300

    draw more attention

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -4,10 +4,38 @@ import { useUser, SignUpButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function HomeHero() {
-  const { isSignedIn } = useUser();
+  const { user, isSignedIn } = useUser();
 
+  const [isPremium, setIsPremium] = useState(false);
+  
+    useEffect(() => {
+      const checkPremiumStatus = async () => {
+        if (!user?.id) return;
+  
+        try {
+          const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/users`, {
+            method: "POST",
+            body: JSON.stringify({ clerkId: user.id }),
+            headers: { "Content-Type": "application/json" },
+          });
+  
+          if (!res.ok) {
+            throw new Error("Failed to fetch user data");
+          }
+  
+          const json = await res.json();
+          setIsPremium(json?.ispremium ?? false);
+        } catch (err) {
+          console.error("Error checking premium status:", err);
+        }
+      };
+  
+      checkPremiumStatus();
+    }, [user]);
+  
   return (
     <section className="text-center space-y-6">
       <motion.section
@@ -29,20 +57,20 @@ export default function HomeHero() {
       <div className="flex justify-center mt-4 gap-4 flex-wrap">
         {isSignedIn ? (
             <Link href={'/dashboard'}>
-          <Button size="lg">
-            Seus Treinos
+          <Button size="lg" className="text-lg mt-2">
+            Ir para o Painel de Treinos
           </Button>
           </Link>
         ) : (
           <SignUpButton mode="modal">
-            <Button size="lg">Começar agora</Button>
+            <Button size="lg" className="text-lg mt-2">Começar agora</Button>
           </SignUpButton>
         )}
-            <Link href={'/precos'}>
-        <Button variant="secondary" size="lg">
+            {!isPremium ? (<Link href={'/precos'}>
+        <Button variant="secondary" size="lg" className="text-lg mt-2"> 
           Ver plano Pro
         </Button>
-        </Link>
+        </Link>) : (<div/>)}
       </div>
     </section>
   );
